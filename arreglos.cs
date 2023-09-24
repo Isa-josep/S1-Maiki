d4dea28e725e32f3b399ba0726277ed092c080ba
Isa-josep <isaurini1902@gmail.com>
2023-09-23 22:38:48 -0600

    vectores/Listas

--- arreglos.cs
+++ arreglos.cs
@@ -2,16 +2,16 @@ using System;
 public class Program{
     public static void Main(){
       Console.Clear();
-        int a,b;
-        Console.WriteLine("Ingrese el tamaño del arreglo");
+        int a,n;
         a=Convert.ToInt32(Console.ReadLine());
-        int[] arr = new int[a];//Declaramos un arreglo de tipo entero de tamaño "A" si se le quiere dar un tamaño sustituir a por el numero 
-        for(int i=0;i<a;i++){// declaramos un for para llenar el arrreglo que va desde 0 hasta a-1 en este caso 
-          b=Convert.ToInt32(Console.ReadLine());//pedimos una variable en este caso es "b"
-          arr[i]=b;//Esta linea dice que lo que esta en b se mete en el indice de i, ejemplo si si b es 5 arr[0] es 5 y asi hasta que se cumpla el for 
+        List<int> b = new List<int>();// ahora toca vectores o listas, esta madre es muy similar al arreglo solo que mas pro 
+        for(int i=0;i<a;i++){//la misma madre para rellenar, un for y queda 
+          n=Convert.ToInt32(Console.ReadLine());
+          b.Add(n);//solo que aqui utilisamos el nombre dle vector/lista y .Add(elem)
         }
-        foreach(int i in arr){// utlizamos el foreach "hack" para imprimir el arreglo, declaramos una variable luego le damos el nombre del arreglo
-          Console.WriteLine(i);// aqui imprimimos i que va a iterar todo el arreglo
+        b.Sort();// una ventaja de los vectores/listas es que se pueden ordenar de una foma muy facil, aqui la pruba 
+        foreach(int i in b){// i para imprimir lo mismo 
+          Console.Write(i+" ");
         }
     }
 }
