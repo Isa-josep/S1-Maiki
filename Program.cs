f4c6e0f9fdc8e3df1ac1dfeb48e96b0848b7b7a5
Isa-josep <isaurini1902@gmail.com>
2023-09-23 18:15:05 -0600

    Calculadora de Salario

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Program{
+    public static void Main(){
+      Console.Clear();
+        int a,b,d;
+        Console.WriteLine("ingrese horas laboradas");
+        a=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("ingrese salario por hora ");
+        b=Convert.ToInt32(Console.ReadLine());
+        d=a*b;
+        Console.WriteLine("Ganaras por dia "+(d)+"\nganas por semana "+(d*7)+"\nganas por mes "+(30*d)+"\nganas por año "+(365*d));
+    }
+}
