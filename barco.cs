a0732413718f74fa39e597d99608d6961c71782c
Isa-josep <98634787+Isa-josep@users.noreply.github.com>
2023-09-29 23:38:26 -0600

    funcion para jugar add, terminado un 95%

--- barco.cs
+++ barco.cs
@@ -2,6 +2,7 @@ using System;
 public class Program{
     struct RandomNumber{
     public int r1, r2;
+    public int t,t1;
     }
     static void RellenarMTX(char[,] matriz, int M,int N ){
         for (int i = 0; i < M; i++){
@@ -18,9 +19,19 @@ public class Program{
           Console.WriteLine("Imposible esas coordenadas no existen");
         }
     }
+
+    static bool searchB(char[,] matriz, int M,int N, int L,int R){
+        if (L >= 0 && L < M && R >= 0 && R < N){
+            if(matriz[L,R]=='B'){
+                return true;
+            } // consideramos que agregara parametros validos
+        }
+        return false;
+    }
     
     public static void Main(){
       Console.Clear();
+      Console.ResetColor();
         int M,N,MAXv,lifep,lifepc,L,R;
         Random random = new Random();
         Console.WriteLine("Ingrese la cantidad de filas del tablero ");
@@ -36,22 +47,19 @@ public class Program{
         lifepc=MAXv;
         Console.WriteLine("Felicidades tienes "+MAXv+" barcos");
         List<RandomNumber> rep = new List<RandomNumber>();
-        
+        List<RandomNumber>repM = new List<RandomNumber>();
         for(int i=1;i<=MAXv;i++){ 
             Console.WriteLine("Ingresa la posision 'Y' en la que quiere agregar un barco");
             L=Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Ingresa la posision 'X' en la que quiere agregar un barco");
             R=Convert.ToInt32(Console.ReadLine());
             AñadirBarcos(mtx1,M,N,L,R); //agregamos los barcos
-            
-            
             int randomNumbeL, randomNumbeR;
             do{
                 randomNumbeL = random.Next(0, M);
                 randomNumbeR = random.Next(0, N);
             } while (rep.Contains(new RandomNumber { r1 = randomNumbeL, r2 = randomNumbeR }));
             rep.Add(new RandomNumber { r1 = randomNumbeL, r2 = randomNumbeR });
-    
            AñadirBarcos(mtx2,M,N,randomNumbeL,randomNumbeR);
             Console.WriteLine("Tiene "+ (MAXv-i) +" barcos");
         }
@@ -78,11 +86,46 @@ public class Program{
         foreach(RandomNumber item in rep){
             Console.WriteLine($"r1: {item.r1}, r2: {item.r2}");
         }
-        // while(lifep !=0 || lifepc!=0){ // Si el jugador o pc tienen sus barcos/vidas el juego continua 
-        //     Console.WriteLine("Ingresa la posision 'Y' en la que quiere buscar  un barco");
-        //     L=Convert.ToInt32(Console.ReadLine());
-        //     Console.WriteLine("Ingresa la posision 'X' en la que quiere buscar un barco");
-        //     R=Convert.ToInt32(Console.ReadLine());
-        // }
+        while (lifep != 0 && lifepc != 0){
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("\t \t Tu turno !!!");
+            Console.ResetColor();
+            Console.WriteLine("Ingresa la posision 'Y' en la que quiere buscar un barco");
+            L = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa la posision 'X' en la que quiere buscar un barco");
+            R = Convert.ToInt32(Console.ReadLine());
+            if (searchB(mtx2, M, N, L, R)){
+                lifepc--;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\t Felicidades encontraste un barco rival!!!");
+                Console.ResetColor();
+            }
+
+            if (lifepc != 0){ 
+                int randomNumbeL, randomNumbeR;
+                do{
+                    randomNumbeL = random.Next(0, M);
+                    randomNumbeR = random.Next(0, N);
+                } while (rep.Contains(new RandomNumber { t = randomNumbeL, t1 = randomNumbeR }));
+                repM.Add(new RandomNumber { t = randomNumbeL, t1 = randomNumbeR });
+                if (searchB(mtx1, M, N, randomNumbeL, randomNumbeR)){
+                    lifep--;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\t !!! Encontraron un barco tuyo, tienes " + lifep + " vidas.");
+                    Console.ResetColor();
+                }
+            }
+        }
+        // El juego ha terminado porque uno de los jugadores se quedó sin vidas
+        if (lifep == 0){
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("¡Has perdido! El PC ha ganado.");
+            Console.ResetColor();
+        }
+        else{
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("¡Has ganado! El PC se quedó sin vidas.");
+            Console.ResetColor();
+        }
     }
 }
